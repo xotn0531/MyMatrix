26c473e99d8d9bbcd1ab70ebd672c2fc8dfe0925
xotn0531 <xotn0531@naver.com>
2024-10-30 13:57:54 +0900

    프로젝트 구조 변경 -리버스 프록시

--- back/src/router/indexRouter.js
+++ back/src/router/indexRouter.js
@@ -1,8 +1,10 @@
+const express = require('express');
+const router = express.Router();
 const jwtMiddleware = require('../jwtMiddleware').jwtMiddleware;
 const indexController = require('../controller/indexController');
 
-exports.indexRouter = function (app) {
-    // 일기 CRUD API
-    app.post("/diary", jwtMiddleware, indexController.createDiary); // create
-    app.get("/diarys", jwtMiddleware, indexController.readDiary);   // read
-};
+// CRUD API
+router.post("/diary", jwtMiddleware, indexController.createDiary); // create
+router.get("/diarys", jwtMiddleware, indexController.readDiary); // read
+
+module.exports = router;
--- back/src/router/userRouter.js
+++ back/src/router/userRouter.js
@@ -1,9 +1,11 @@
-const userController = require("../controller/userController");
-const { jwtMiddleware } = require("../jwtMiddleware");
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/userController');
+const jwtMiddleware = require('../jwtMiddleware').jwtMiddleware;
 
-exports.userRouter = function (app) {
-    // 회원가입 API
-    app.post("/signup", userController.signup);
-    // 로그인 API
-    app.post("/signin", userController.signin);
-};
+// 회원가입 API
+router.post("/signup", userController.signup);
+// 로그인 API
+router.post("/signin", userController.signin);
+
+module.exports = router;
